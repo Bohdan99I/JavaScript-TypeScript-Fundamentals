f98cccaae256ba0be6b04a40519a144e7e0571f1
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-06-16 15:50:59 +0300

    add_2. [ADVANCED-CR] LOOPS. FUNCTIONS

---
+++ examples/2. [ADVANCED-CR] LOOPS. FUNCTIONS.js
@@ -0,0 +1,8 @@
+//Create a function generatePrimes that takes in a number n as an argument.
+//This function should generate and return an array containing the first n prime numbers.
+
+//Write another function isPrime that takes a number num as an argument and determines
+//whether it's a prime number or not. This function should return true if the number
+//is prime and false otherwise.
+
+//Utilize these functions to generate an array of the first n prime numbers.
